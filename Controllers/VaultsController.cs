d083f0064ece73ef0380c2ec270c71b6aed96661
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-10-23 16:06:13 -0600

    vaults controller

--- Controllers/VaultsController.cs
+++ Controllers/VaultsController.cs
@@ -1,7 +1,67 @@
+using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using Keepr.Models;
+using Keepr.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Keepr.Controllers
 {
-    public class VaultsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VaultsController : ControllerBase
     {
-        
+        private readonly VaultsService _service;
+        public VaultsController(VaultsService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vault>> GetVaultById(int id)
+        {
+            try
+            {
+                Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+                return Ok(_service.GetById(userInfo.Id, id))
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<Vault>> CreateVault([FromBody] Vault newVault)
+        {
+            try
+            {
+                Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+                newVault.CreatorId = userInfo.Id;
+                Vault created = _service.CreateVault(newVault);
+                created.Creator = userInfo;
+                return Ok(created);
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<ActionResult<string>> DeleteVault(int id)
+        {
+            try
+            {
+                Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+                return Ok(_service.DeleteVault(id, userInfo.Id));
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
--- Services/VaultsService.cs
+++ Services/VaultsService.cs
@@ -1,7 +1,23 @@
+using System;
+using Keepr.Models;
+
 namespace Keepr.Services
 {
     public class VaultsService
     {
+        internal object GetById(string id1, int id2)
+        {
+            throw new NotImplementedException();
+        }
 
+        internal Vault CreateVault(Vault newVault)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object DeleteVault(int id1, string id2)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
