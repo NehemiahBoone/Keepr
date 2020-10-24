34feeafca68124a70ccdfeb9ae968d598fb7ea58
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-10-24 14:44:51 -0600

    vaultkeepscontroller

--- Controllers/VaultKeepsController.cs
+++ Controllers/VaultKeepsController.cs
@@ -1,7 +1,45 @@
+using Keepr.Models;
+using Keepr.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Keepr.Controllers
 {
-    public class VaultKeepsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VaultKeepsController : ControllerBase
     {
-        
+        private readonly VaultKeepsService _service;
+        public VaultKeepsController(VaultKeepsService service)
+        {
+            _service = service;
+        }
+
+    [HttpPost]
+    public ActionResult<string> CreateVaultKeep([FromBody] VaultKeep newVK)
+    {
+        try
+        {
+            _service.CreateVaultKeep(newVK);
+            return Ok("Created vaultkeep... from vaultKeepsController");
+        }
+        catch (System.Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<string> DeleteVaultKeep(int id)
+    {
+        try
+        {
+            _service.DeleteVaultKeep(id);
+            return Ok("Deleted vaultkeep... from vaultKeepsController");
+        }
+        catch (System.Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
     }
 }
--- Services/VaultKeepsService.cs
+++ Services/VaultKeepsService.cs
@@ -1,7 +1,18 @@
+using System;
+using Keepr.Models;
+
 namespace Keepr.Services
 {
     public class VaultKeepsService
     {
-        
+        internal void CreateVaultKeep(VaultKeep newVK)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void DeleteVaultKeep(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
