55586e5640b1a631a3922c4b583ded9703f14787
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-10-23 18:26:25 -0600

    vaults getbyid

--- Repositories/VaultsRepository.cs
+++ Repositories/VaultsRepository.cs
@@ -1,13 +1,29 @@
 using System;
+using System.Data;
+using Dapper;
 using Keepr.Models;
 
 namespace Keepr.Repositories
 {
     public class VaultsRepository
     {
+        private readonly IDbConnection _db;
+
+        private readonly string populateCreator = @"SELECT
+            vault.*,
+            profile.*
+            FROM vaults vault
+            JOIN profiles profile on vault.creatorId = profile.Id ";
+        
+        public VaultsRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+        
         internal Vault GetById(int id)
         {
-            throw new NotImplementedException();
+            string sql = populateCreator + "WHERE vault.id = @id";
+            return _db.Query<Vault, Profile, Vault>(sql, (vault, profile) => {vault.Creator = profile; return vault}, new {id}, splitOn: "id").FirstOrDefault();
         }
 
         internal int Create(Vault newVault)
